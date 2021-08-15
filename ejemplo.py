23daecd750697246e135dde73e37019d08baa8b5
Rosa Severiche <severicherosa96@gmail.com>
2021-08-15 19:03:05 -0300

    ejemplo.py

---
+++ ejemplo.py
@@ -0,0 +1,100 @@
+import os
+import qrcode
+from telegram.ext import Updater, CommandHandler, ConversationHandler, CallbackQueryHandler, MessageHandler, Filters
+from telegram import ChatAction, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+INPUT_TEXT = 0
+
+
+def start(update, context):
+
+   update.message.reply_text(
+        text='Bienvenido a bordo, \n\nUsa /qr para generar un código qr.',
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton(text='Generar qr', callback_data='qr')],
+            [InlineKeyboardButton(text='Aviación Comecial', url='https://www.aviacionline.com/category/aviacion-comercial/')],
+            [InlineKeyboardButton(text='Aviación Militar', url='https://www.aviacionline.com/category/aviacion-militar/')],
+            [InlineKeyboardButton(text='Aviación Ejecutiva', url='https://www.aviacionline.com/category/aviacion-ejecutiva/')]
+        ])
+    )
+
+def qr_command_handler(update, context):
+
+    update.message.reply_text('Envíame el texto para generarte un código QR')
+
+    return INPUT_TEXT
+
+
+def qr_callback_handler(update, context):
+
+    query = update.callback_query
+    query.answer()
+
+    query.edit_message_text(
+        text='Envíame el texto para generarte un código QR'
+    )
+
+    return INPUT_TEXT
+
+
+def generate_qr(text):
+
+    filename = text + '.jpg'
+
+    img = qrcode.make(text)
+    img.save(filename)
+
+    return filename
+
+
+def send_qr(filename, chat):
+
+    chat.send_action(
+        action=ChatAction.UPLOAD_PHOTO,
+        timeout=None
+    )
+
+    chat.send_photo(
+        photo=open(filename, 'rb')
+    )
+
+    os.unlink(filename)
+
+
+def input_text(update, context):
+
+    text = update.message.text
+
+    filename = generate_qr(text)
+
+    chat = update.message.chat
+
+    send_qr(filename, chat)
+
+    return ConversationHandler.END
+
+
+if __name__ == '__main__':
+
+    updater = Updater(token='1915329311:AAGP0EM_2_Lj_iwDGO5Y1iiK0j9_F8MbPjs', use_context=True)
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start', start))
+
+    dp.add_handler(ConversationHandler(
+        entry_points=[
+            CommandHandler('qr', qr_command_handler),
+            CallbackQueryHandler(pattern='qr', callback=qr_callback_handler)
+        ],
+
+        states={
+            INPUT_TEXT: [MessageHandler(Filters.text, input_text)]
+        },
+
+        fallbacks=[]
+    ))
+
+    updater.start_polling()
+    updater.idle()
